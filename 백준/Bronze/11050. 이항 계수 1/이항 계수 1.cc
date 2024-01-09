437eecf951a0c1789b2d80b06bac6cd230b32e04
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-01-09 20:17:05 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if(n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << factorial(n) / (factorial(k) * factorial(n - k)) << endl;
+
+    return 0;
+}
+
+
