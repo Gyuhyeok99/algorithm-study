f27a2e64db5b70909be3de5b866864d927d87968
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-01-09 18:55:45 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <cmath>
+
+#define M 1234567891
 using namespace std;
 
 int main() {
+
     int n;
     cin >> n;
+    long long hash = 0, r = 1;
     string s;
     cin >> s;
-    int arr[50];
-    int sum = 0;
-    for(int i = 0; i < n; i++) {
-        arr[i] = s[i] - 96;
-        sum += arr[i] * pow(31, i);
+    for(int i = 0; i < s.size(); i++) {
+        hash = (hash + (s[i] - 'a' + 1) * r ) % M;
+        r = (r * 31) % M;
     }
-    int result  = sum % 1234567891;
-    cout << result << endl;
-    return 0;
+    cout << hash << endl;
+
 }
