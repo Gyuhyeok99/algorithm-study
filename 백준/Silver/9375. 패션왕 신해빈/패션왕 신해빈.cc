17d498709f4ab8f15ad7cfdbbe753364a9a849e7
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-02-11 23:43:39 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -11,12 +11,7 @@ int main() {
         cin >> n;
         for(int j = 0; j < n; j++) {
             cin >> a >> b;
-            if(mp.find(b) != mp.end()) {
-                mp[b]++;
-            }
-            else {
-                mp[b] = 1;
-            }
+            mp[b]++;
         }
         int x = 1;
         for(const auto& pair : mp) {
