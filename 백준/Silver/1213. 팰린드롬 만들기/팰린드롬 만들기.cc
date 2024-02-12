403c3b708b2bcc523f58c899d58143e1de9a1b2c
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-02-12 13:52:14 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -2,44 +2,44 @@
 #include <string.h>
 using namespace std;
 
+int cnt[26], flag;
+string s, ret;
+char mid;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    memset(cnt, 0, sizeof(cnt));
 
-    string s;
-    string ret;
-    int arr[26];
-    int flag = 0;
-    char mid;
-    memset(arr, 0, sizeof(arr));
     cin >> s;
     for(int i = 0; i < s.size(); i++) {
-        arr[s[i] - 'A']++;
+        cnt[s[i] - 'A']++;
     }
 
-    for(int i = 25; i >= 0; i--) {
-        if(arr[i]) {
-            if(arr[i] & 1) {
-                mid = i + 'A';
+    for(int i =  25; i >= 0; i--) {
+        if(cnt[i]) {
+            if(cnt[i] & 1) {
                 flag++;
-                arr[i]--;
+                mid = i + 'A';
+                cnt[i]--;
             }
             if(flag == 2) {
                 break;
             }
-            for(int j = 0; j < arr[i]; j +=2) {
-                ret = (char)(i + 'A') + ret;
+            for(int j = 0; j < cnt[i]; j += 2) {
+                ret = char(i + 'A') + ret;
                 ret += char(i + 'A');
             }
         }
     }
-
     if(flag == 2) {
-        cout <<  "I'm Sorry Hansoo";
+        cout << "I'm Sorry Hansoo" << '\n';
         return 0;
     }
-    else if(flag == 1) {
+    if(flag == 1) {
         ret.insert(ret.begin() + ret.size()/ 2, mid);
     }
     cout << ret << '\n';
+
+
+    return 0;
 }
