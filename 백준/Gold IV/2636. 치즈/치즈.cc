1ddb34532995649aa2405a8e02394630936a2ce7
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-02-16 12:21:51 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold IV/2636. 치즈/치즈.cc
+++ 백준/Gold IV/2636. 치즈/치즈.cc
@@ -5,8 +5,9 @@ using namespace std;
 
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int n, m, a[101][101], visited[101][101], ret, cnt;
 vector<pair<int, int>> v;
+int n, m, ret, cnt, a[101][101], visited[101][101];
+
 void dfs(int y, int x) {
     visited[y][x] = 1;
     if(a[y][x] == 1) {
@@ -16,7 +17,7 @@ void dfs(int y, int x) {
     for(int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) {
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] == 1) {
             continue;
         }
         dfs(ny, nx);
@@ -26,6 +27,7 @@ void dfs(int y, int x) {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
@@ -36,11 +38,11 @@ int main() {
     while(true) {
         memset(visited, 0, sizeof(visited));
         v.clear();
-        bool flag = 1;
         dfs(0, 0);
+        bool flag = 1;
         cnt = v.size();
-        for(auto i : v) {
-            a[i.first][i.second] = 0;
+        for(auto it : v) {
+            a[it.first][it.second] = 0;
         }
 
         for(int i = 0; i < n; i++) {
@@ -55,7 +57,5 @@ int main() {
             break;
         }
     }
-    cout << ret << '\n';
-    cout << cnt << '\n';
-    return 0;
+    cout << ret << '\n' << cnt << '\n';
 }
