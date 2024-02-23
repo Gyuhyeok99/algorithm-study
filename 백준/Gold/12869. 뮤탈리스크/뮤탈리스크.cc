c7695c0bfc8ad796902f2ee2c587e72bec331496
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-02-23 17:19:50 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3176 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,25 +1,28 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
 using namespace std;
-
-
-struct A {
-    int a, b, c;
-};
 int n, a[3], visited[64][64][64];
-int _a[6][3]{
+int _a[6][3] = {
         {9, 3, 1},
         {9, 1, 3},
         {3, 9, 1},
         {3, 1, 9},
-        {1, 9 ,3},
+        {1, 9, 3},
         {1, 3, 9}
 };
+struct A{
+    int a, b, c;
+};
 queue<A> q;
-int solve(int a, int b, int c) {
-    visited[a][b][c] = 1;
-    q.push({a, b, c});
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    q.push({a[0], a[1], a[2]});
+    visited[a[0]][a[1]][a[2]] = 1;
     while(q.size()) {
         int a = q.front().a;
         int b = q.front().b;
@@ -36,18 +39,9 @@ int solve(int a, int b, int c) {
                 continue;
             }
             visited[na][nb][nc] = visited[a][b][c] + 1;
-            q.push({na, nb ,nc});
+            q.push({na, nb, nc});
         }
     }
-    return visited[0][0][0] - 1;
-}
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    cout << solve(a[0], a[1], a[2]) << '\n';
+    cout << visited[0][0][0] - 1 << '\n';
     return 0;
 }
