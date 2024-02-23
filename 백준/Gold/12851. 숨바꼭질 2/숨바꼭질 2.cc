2dec27d25c147ea012767a066cae09000431e0c9
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-02-23 17:33:01 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 4 ms, Memory: 3848 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,34 +1,33 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-const int MAX = 200000;
-int n, k, visited[MAX + 4], cnt[MAX + 4];
+
+int n, k, ret, visited[200004], cnt[200004];
 queue<int> q;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     cin >> n >> k;
     if(n == k) {
         cout << 0 << '\n' << 1 << '\n';
         return 0;
     }
-    visited[n] = 1;
     q.push(n);
+    visited[n] = 1;
     cnt[n] = 1;
     while(q.size()) {
         int x = q.front(); q.pop();
-        for(int next : {x + 1, x - 1, 2 * x}) {
-            if(next < 0 || next > MAX) {
+        for(int nx : {x + 1, x - 1, x * 2}) {
+            if(nx < 0 || nx >= 200004) {
                 continue;
             }
-            if(visited[next] == 0) {
-                q.push(next);
-                visited[next] = visited[x] + 1;
-                cnt[next] += cnt[x];
+            if(visited[nx] == 0) {
+                q.push(nx);
+                visited[nx] = visited[x] + 1;
+                cnt[nx] += cnt[x];
             }
-            else if(visited[next] == visited[x] + 1) {
-                cnt[next] += cnt[x];
+            else if(visited[nx] == visited[x] + 1) {
+                cnt[nx] += cnt[x];
             }
         }
     }
