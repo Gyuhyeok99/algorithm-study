f59fdba1bb758e3a357d6363d2352c9acf8df3f3
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-08-12 15:19:46 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,33 +1,32 @@
 #include <iostream>
-#include <string.h>
 #include <vector>
 using namespace std;
 
-int a[26];
-string s[151];
 int n;
+string s;
+int arr[26];
+vector<char> v;
 int main() {
-    std::ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    memset(a, 0, sizeof(a));
 
     cin >> n;
     for(int i = 0; i < n; i++) {
-        cin >> s[i];
-        a[s[i][0] - 'a']++;
+        cin >> s;
+        arr[s[0] - 'a']++;
     }
-    vector<char> v;
     for(int i = 0; i < 26; i++) {
-        if(a[i] >= 5) {
+        if(arr[i] >= 5) {
             v.push_back(i + 'a');
         }
     }
-    if(v.size() == 0) {
-        cout << "PREDAJA" << '\n';
-    }
-    else {
-        for (char c: v) {
+    if(v.size()) {
+        for(char c : v) {
             cout << c;
         }
     }
+    else {
+        cout << "PREDAJA" << '\n';
+    }
+    return 0;
 }
