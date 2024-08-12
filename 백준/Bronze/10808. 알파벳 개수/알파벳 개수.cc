753b49bfb5eb2464237c293c53879c69b27ea13d
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-08-12 14:42:10 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -2,17 +2,17 @@
 using namespace std;
 
 string s;
-int arr[26];
+int a[26];
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
+    cin.tie(NULL);cout.tie(NULL);
     cin >> s;
     for(int i = 0; i < s.size(); i++) {
-        arr[s[i] - 'a']++;
+        a[s[i] - 97]++;
     }
-    for(int a : arr) {
-        cout << a << ' ';
+    for(int i : a) {
+        cout << i << ' ';
     }
+
     return 0;
 }
