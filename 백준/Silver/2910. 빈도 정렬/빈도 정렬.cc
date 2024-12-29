5123e75ebabe8faf403f2481251230197af29e73
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-12-29 20:06:09 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,38 +1,38 @@
 #include <iostream>
+#include <vector>
 #include <map>
 #include <algorithm>
-#include <vector>
 using namespace std;
-int n, c, a[1004];
+int n, c;
+map<int, int> mp1, mp2;
 vector<pair<int, int>> v;
-map<int, int> mp, mp_first;
+
 bool cmp(pair<int, int> a, pair<int, int> b) {
-    if(a.first == b.first) {
-        return mp_first[a.second] < mp_first[b.second];
+    if(a.second == b.second) {
+        return mp2[a.first] < mp2[b.first];
     }
-    return a.first > b.first;
+    return a.second > b.second;
 }
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
+    cin.tie(NULL); cout.tie(NULL);
     cin >> n >> c;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-        mp[a[i]]++;
-        if(mp_first[a[i]] == 0) {
-            mp_first[a[i]] = i + 1;
+    for(int i = 0; i < n ; i++) {
+        int x;
+        cin >> x;
+        mp1[x]++;
+        if(mp2[x] == 0) {
+            mp2[x] = i + 1;
         }
-    }
-    for(auto it : mp) {
-        v.push_back({it.second, it.first});
-    }
 
+    }
+    for(auto temp : mp1) {
+        v.push_back({temp.first, temp.second});
+    }
     sort(v.begin(), v. end(), cmp);
-
-    for(auto i : v) {
-        for(int j = 0; j < i.first; j++) {
-            cout << i.second << " ";
+    for(auto temp : v) {
+        for(int i = 0; i < temp.second; i++) {
+            cout << temp.first << ' ';
         }
     }
     return 0;
