001f97c8cecf06d0b6bd92caff5555ed6069bf25
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-12-31 16:24:58 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,23 +1,30 @@
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
+int t;
 
+int solve(int n) {
+    int cnt_5 = 0;
+    int cnt_2 = 0;
+    for(int i = 2; i <= n; i *= 2) {
+        cnt_2 += n / i;
+    }
+    for(int i = 5; i <= n; i *= 5) {
+        cnt_5 += n / i;
+    }
+    return min(cnt_2, cnt_5);
+}
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int t, n, cnt_2, cnt_5;
+    cin.tie(NULL); cout.tie(NULL);
+
     cin >> t;
     for(int i = 0; i < t; i++) {
+        int n;
         cin >> n;
-        cnt_2 = 0; cnt_5 = 0;
-        for(int j = 2; j <= n; j *= 2) {
-            cnt_2 += n / j;
-        }
-        for(int j = 5; j <= n; j *= 5) {
-            cnt_5 += n / j;
-        }
-        cout << min(cnt_2, cnt_5) << '\n';
+        cout << solve(n) << '\n';
     }
+    return 0;
 }
