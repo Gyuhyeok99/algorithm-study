76470bfde8ca3ec729531b90589303a43fb3ccf4
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-12-31 23:46:10 +0900

    [Gold IV] Title: 오큰수, Time: 204 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,27 +1,27 @@
 #include <iostream>
-#include <string.h>
 #include <stack>
+#include <cstring>
 using namespace std;
 
-stack<int> s;
-int n, a[1000001], ret[1000001];
+int a[1000004], b[1000004];
+int n;
+stack<int> st;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     cin >> n;
-    memset(ret, -1, sizeof(ret));
+    memset(b, -1, sizeof(b));
     for(int i = 0; i < n; i++) {
         cin >> a[i];
-        while(s.size() && a[s.top()] < a[i]) {
-            ret[s.top()] = a[i];
-            s.pop();
+        while(st.size() && a[st.top()] < a[i]) {
+            b[st.top()] = a[i];
+            st.pop();
         }
-        s.push(i);
+        st.push(i);
     }
     for(int i = 0; i < n; i++) {
-        cout << ret[i] << ' ';
+        cout << b[i] << ' ';
     }
     return 0;
 }
