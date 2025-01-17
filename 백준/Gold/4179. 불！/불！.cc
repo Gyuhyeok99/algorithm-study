d563e3b987af10ae35096f7adeb20231bf3117d0
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-17 09:36:21 +0900

    [Gold III] Title: 불!, Time: 32 ms, Memory: 10964 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,63 +1,85 @@
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <queue>
 using namespace std;
-const int INF = 987654321;
-char a[1004][1004];
-int n,m,sx,sy,dx[4]={-1,0,1,0},dy[4]={0,-1,0,1}, ret, y, x;
-int fire_check[1004][1004], person_check[1004][1004];
-bool in(int a,int b){
-	return 0 <= a && a < n && 0 <= b && b < m;
-}
-int main(){
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+char arr[1001][1001];
+int jihun[1001][1001], fire[1001][1001];
+int r, c, ret;
+pair<int, int> s_ji;
+queue<pair<int, int>> q;
+bool flag = false;
+int main() {
     ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0); 
-	queue<pair<int, int>> q;
-    cin >> n >> m;  
-	fill(&fire_check[0][0], &fire_check[0][0] + 1004 * 1004, INF);
-    for(int i = 0; i < n; i++){ 
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-            if(a[i][j] == 'F'){
-                fire_check[i][j] = 1; q.push({i, j});
-            }else if(a[i][j] == 'J'){
-                sy = i; sx = j;
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> r >> c;
+    for(int i = 0; i < r; i++) {
+        string s;
+        cin >> s;
+        for(int j = 0; j < c; j++) {
+            arr[i][j] = s[j];
+            if(s[j] == 'J') {
+                s_ji = {i, j};
+            }
+            else if(s[j] == 'F') {
+                q.push({i, j});
+                fire[i][j] = 1;
             }
         }
-    } 
+    }
 
-	while(q.size()){
-        tie(y, x) = q.front(); 
-		q.pop();
-		for(int i = 0; i < 4; i++){
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(!in(ny,nx)) continue;
-			if(fire_check[ny][nx] != INF || a[ny][nx]=='#') continue;
-			fire_check[ny][nx] = fire_check[y][x] + 1;
-			q.push({ny, nx});
-		}
-	}
-
-	person_check[sy][sx]=1;
-	q.push({sy,sx});
-	while(q.size()){
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop(); 
-		if(x == m - 1 || y == n - 1 || x == 0 || y == 0){
-            ret = person_check[y][x];
-            break;
-		}
-		for(int i = 0; i < 4; i++){
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(!in(ny,nx)) continue;
-			if(person_check[ny][nx] || a[ny][nx]=='#') continue; 
-			if(fire_check[ny][nx] <= person_check[y][x] + 1) continue;
-            person_check[ny][nx] = person_check[y][x] + 1;
+    while(q.size()) {
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= r || nx >= c) {
+                continue;
+            }
+            if(arr[ny][nx] == '#' || fire[ny][nx] != 0) {
+                continue;
+            }
+            fire[ny][nx] = fire[y][x] + 1;
             q.push({ny, nx});
-		}
-	} 
-    if(ret != 0) cout << ret << "\n";
-	else cout << "IMPOSSIBLE \n";
-	return 0;
+        }
+    }
+
+    q.push({s_ji.first, s_ji.second});
+    jihun[s_ji.first][s_ji.second] = 1;
+    while(q.size()) {
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        if(y == 0 || x == 0 || y == r - 1 || x == c - 1) {
+            ret = jihun[y][x];
+            flag = true;
+            break;
+        }
+        for(int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= r || nx >= c) {
+                continue;
+            }
+            if(arr[ny][nx] == '#' || jihun[ny][nx] != 0) {
+                continue;
+            }
+            if(fire[ny][nx] && jihun[y][x] + 1 >= fire[ny][nx]) {
+                continue;
+            }
+            jihun[ny][nx] = jihun[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+
+    if(flag) {
+        cout << ret << '\n';
+    }else {
+        cout << "IMPOSSIBLE" << '\n';
+    }
+    return 0;
 }
