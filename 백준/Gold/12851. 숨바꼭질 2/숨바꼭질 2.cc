9b5863b388257ba0dbe633375b341fdcebbdad90
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-20 11:14:49 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 4 ms, Memory: 3848 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-
-int n, k, ret, visited[200004], cnt[200004];
+int n, k;
+int visited[200002], cnt[200002];
 queue<int> q;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
     cin >> n >> k;
     if(n == k) {
         cout << 0 << '\n' << 1 << '\n';
@@ -16,12 +17,13 @@ int main() {
     visited[n] = 1;
     cnt[n] = 1;
     while(q.size()) {
-        int x = q.front(); q.pop();
-        for(int nx : {x + 1, x - 1, x * 2}) {
-            if(nx < 0 || nx >= 200004) {
+        int x = q.front();
+        q.pop();
+        for(int nx : {x - 1, x + 1, 2 * x}) {
+            if(nx < 0 || nx >= 200002) {
                 continue;
             }
-            if(visited[nx] == 0) {
+            if(!visited[nx]) {
                 q.push(nx);
                 visited[nx] = visited[x] + 1;
                 cnt[nx] += cnt[x];
@@ -31,7 +33,6 @@ int main() {
             }
         }
     }
-    cout << visited[k] - 1 << '\n';
-    cout << cnt[k] << '\n';
+    cout << visited[k] - 1 << '\n' << cnt[k] << '\n';
     return 0;
 }
