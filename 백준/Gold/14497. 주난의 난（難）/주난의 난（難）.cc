3ba81c2c5c42a11627c4d5ac2b5b59d50525fbfc
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-21 11:05:58 +0900

    [Gold IV] Title: 주난의 난(難), Time: 4 ms, Memory: 2608 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 #include <tuple>
 using namespace std;
-#define y1 aaaa
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-int n, m, x1, x2, y1, y2, x, y, ret, visited[301][301];
-char a[301][301];
-queue<pair<int, int>> q;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int n, m, x1, _y1, x2, y2, y, x, ret;
+char arr[301][301];
+int visited[301][301];
+queue<pair<int, int>>q;
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> n >> m;
-    cin >> y1 >> x1 >> y2 >> x2;
-    x1--, x2--, y1--, y2--;
+    cin >> _y1 >> x1 >> y2 >> x2;
+    _y1--, x1--, y2--, x2--;
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin >> a[i][j];
+            cin >> arr[i][j];
         }
     }
-
-    q.push({y1, x1});
-    visited[y1][x1] = 1;
-    while(a[y2][x2] != '0') {
+    q.push({_y1, x1});
+    visited[_y1][x1] = 1;
+    while(arr[y2][x2] != '0') {
         ret++;
         queue<pair<int, int>> temp;
         while(q.size()) {
-            tie(y, x) = q.front(); q.pop();
-            for (int i = 0; i < 4; i++) {
+            tie(y, x) = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) {
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) {
                     continue;
                 }
                 visited[ny][nx] = ret;
-                if (a[ny][nx] != '0') {
-                    a[ny][nx] = '0';
-                    temp.push({ny, nx});
-                } else {
+                if(arr[ny][nx] != '0') {
+                    arr[ny][nx] = '0';
+                    temp.push({ny,nx});
+                }else {
                     q.push({ny, nx});
                 }
             }
         }
         q = temp;
     }
-    cout << visited[y2][x2] << '\n';
-    return 0;
+    cout << ret << '\n';
 }
