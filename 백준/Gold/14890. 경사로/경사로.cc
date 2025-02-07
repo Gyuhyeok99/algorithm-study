0b3063b2d8e2f1d6404e2326a051f3b9f97c66a3
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-02-07 17:29:26 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,29 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std; 
-int n, l, a[104][104], b[104][104], ret; 
-void solve(int a[104][104]){
-    for(int i = 0; i < n; i++){
-        int cnt = 1; 
-        int j;  
-        for(j = 0; j < n - 1; j++){
-            if(a[i][j] == a[i][j + 1])cnt++; 
-            else if(a[i][j] + 1 == a[i][j + 1] && cnt >= l) cnt = 1; 
-            else if(a[i][j] - 1 == a[i][j + 1] && cnt >= 0) cnt = -l + 1; 
-            else break;
+#include <iostream>
+using namespace std;
+
+int n, l, ret;
+int a[101][101], b[101][101];
+
+void solve(int x[101][101]) {
+    for(int i = 0; i < n; i++) {
+        int cnt = 1;
+        int j;
+        for(j = 0; j < n - 1; j++) {
+            if(x[i][j] == x[i][j + 1]) {
+                cnt++;
+            }
+            else if(x[i][j] + 1 == x[i][j + 1] && cnt >= l) {
+                cnt = 1;
+            }
+            else if(x[i][j] - 1 == x[i][j + 1] && cnt >= 0) {
+                cnt = -l + 1;
+            }
+            else {
+                break;
+            }
         }
-        if(j == n - 1 && cnt >= 0) ret++; 
+        if(j == n - 1 && cnt >= 0) {
+            ret++;
+        }
     }
-    return; 
-} 
-int main(){
-    scanf("%d %d", &n, &l);
-    for(int i = 0; i < n; i++){ 
-        for(int j = 0; j < n; j++){
-            scanf("%d", &a[i][j]); 
+    return;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> l;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j];
             b[j][i] = a[i][j];
         }
-    }    
-    solve(a); solve(b);  
-    printf("%d\n", ret);  
-    return 0; 
+    }
+
+    solve(a);
+    solve(b);
+    cout << ret << '\n';
+    return 0;
 }
