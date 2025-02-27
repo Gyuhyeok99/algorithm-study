c66c1029173a511e3c77825a66949b1064d61d7b
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-02-27 19:59:16 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 156.11 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -3,12 +3,13 @@
 #include <queue>
 
 using namespace std;
-int inde[1000005];  
-int outde[1000005]; 
-int root = -1;
-vector<int> v[1000005];
+int inde[1000004];
+int outde[1000004];
+vector<int> adj[1000004];
+int root;
+
 int findRoot() {
-    for(int i = 0; i < 1000005; i++) {
+    for(int i = 0; i < 1000004; i++) {
         if(inde[i] == 0 && outde[i] >= 2) {
             return i;
         }
@@ -18,46 +19,48 @@ int findRoot() {
 
 int bfs(int here) {
     queue<int> q;
-    
-    if(v[here].size() != 1) {
-        if(v[here].size() == 0) {
+    if(adj[here].size() != 1) {
+        if(adj[here].size() == 0) {
             return 2;
         }
         else {
             return 3;
         }
     }
-    q.push(v[here][0]);
+    q.push(adj[here][0]);
     while(q.size()) {
         int there = q.front();
         q.pop();
-        if(v[there].size() != 1) {
-            if(v[there].size() == 0) {
+        if(adj[there].size() != 1) {
+            if(adj[there].size() == 0) {
                 return 2;
             }
             else {
                 return 3;
             }
         }
-        if(there == here) {
+        if(here == there) {
             return 1;
         }
-        q.push(v[there][0]);
+        q.push(adj[there][0]);
     }
 }
-
 vector<int> solution(vector<vector<int>> edges) {
     vector<int> answer = {0, 0, 0, 0};
+    // 그래프 입력
     for(int i = 0; i < edges.size(); i++) {
-        v[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][0]].push_back(edges[i][1]);
         inde[edges[i][1]]++;
         outde[edges[i][0]]++;
     }
+    
+    // 루트 찾기
     root = findRoot();
     answer[0] = root;
     
-    for(int i = 0; i < v[root].size(); i++) {
-        int idx = bfs(v[root][i]);
+    // 그래프 찾기
+    for(int i = 0; i < adj[root].size(); i++) {
+        int idx = bfs(adj[root][i]);
         answer[idx]++;
     }
     return answer;
