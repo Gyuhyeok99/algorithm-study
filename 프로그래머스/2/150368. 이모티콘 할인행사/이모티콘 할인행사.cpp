24d9a1f4cab08c2c1563bfae4f5fae78f17ddcb1
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-02-28 11:54:54 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 19.42 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -4,49 +4,48 @@
 
 using namespace std;
 
-int discount[] = {10, 20, 30, 40};
+const int discount[] = {10, 20, 30, 40};
 vector<int> answer(2);
+vector<vector<int>> c_users;
+vector<int> c_emoticons;
 vector<pair<int, int>> price(7);
-vector<vector<int>> u;
-vector<int> e;
 
 void dfs(int idx, int n) {
     if(idx == n) {
-        int plus = 0;
-        int sum = 0;
-        for(int i = 0; i < u.size(); i++) {
-            int tmp = 0;
-            for(int j = 0; j < n; j++) {
-                if(u[i][0] <= price[j].second) {
-                    tmp += price[j].first;
+        int plus = 0, sum = 0;
+        for(int i = 0; i < c_users.size(); i++) {
+            int temp = 0;
+            for(int j = 0; j < c_emoticons.size(); j++) {
+                if(c_users[i][0] <= price[j].first) {
+                    temp += price[j].second;
                 }
             }
-            if(u[i][1] <= tmp) {
+            if(c_users[i][1] <= temp) {
                 plus++;
             }
             else {
-                sum += tmp;
-            } 
+                sum += temp; 
+            }
         }
         if(answer[0] < plus) {
             answer[0] = plus;
             answer[1] = sum;
         }
         else if(answer[0] == plus) {
-            answer[1] = max(sum, answer[1]);
+            answer[1] = max(answer[1], sum);
         }
         return;
     }
     for(int i = 0; i < 4; i++) {
-        price[idx].first = (100 - discount[i]) * e[idx] / 100;
-        price[idx].second = discount[i];
+        price[idx].first = discount[i];
+        price[idx].second = (100 - discount[i]) * c_emoticons[idx] / 100;
         dfs(idx + 1, n);
     }
 }
 
 vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
-    u = users;
-    e = emoticons;
-    dfs(0, e.size());
+    c_users = users;
+    c_emoticons = emoticons;
+    dfs(0, c_emoticons.size());
     return answer;
 }
