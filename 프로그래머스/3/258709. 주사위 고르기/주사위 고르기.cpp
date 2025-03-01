fa3d735115757a9ebd717c0f4dbbee66fc6ddc74
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-03-01 13:00:53 +0900

    [level 3] Title: 주사위 고르기, Time: 125.52 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.cpp
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.cpp
@@ -1,45 +1,45 @@
 #include <string>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<vector<int>> d;
 int n, k;
+int max_cnt;
 vector<int> answer;
-int maxCount;
-
-void getSum(int idx, int sum, vector<int>& selected, vector<int>& s) {
-    if(idx == selected.size()) {
-        s.push_back(sum);
+void getSum(int idx, int sum, vector<int>& selectedX, vector<int>& sumX) {
+    if(idx == selectedX.size()) {
+        sumX.push_back(sum);
         return;
     }
     for(int i = 0; i < 6; i++) {
-        getSum(idx + 1, sum + d[selected[idx]][i], selected, s);
+        getSum(idx + 1, sum + d[selectedX[idx]][i] , selectedX, sumX);
     }
+    return;
 }
 
 void combi(int start, vector<int> selectedA) {
     if(selectedA.size() == k) {
-        //종료 시 해야할 조건
         vector<int> selectedB;
         for(int i = 0; i < n; i++) {
             if(find(selectedA.begin(), selectedA.end(), i) == selectedA.end()) {
                 selectedB.push_back(i);
             }
         }
-        vector<int> sumA, sumB;
+        vector<int> sumA;
+        vector<int> sumB;
         getSum(0, 0, selectedA, sumA);
         getSum(0, 0, selectedB, sumB);
         
         sort(sumB.begin(), sumB.end());
-        
-        int count = 0;
-        for(int i : sumA) {
-            count += lower_bound(sumB.begin(), sumB.end(), i) - sumB.begin();
+        int cnt = 0;
+        for(int a : sumA) {
+            cnt += lower_bound(sumB.begin(), sumB.end(), a) - sumB.begin();
         }
-        if(count > maxCount) {
-            maxCount = count;
+        
+        if(cnt > max_cnt) {
+            max_cnt = cnt;
             answer = selectedA;
         }
         return;
@@ -49,18 +49,17 @@ void combi(int start, vector<int> selectedA) {
         combi(i, selectedA);
         selectedA.pop_back();
     }
+    return;
 }
 
 vector<int> solution(vector<vector<int>> dice) {
     d = dice;
-    n = dice.size();
+    n = d.size();
     k = n / 2;
-    
     combi(-1, {});
     
     for(int & i : answer) {
         i++;
     }
-    
     return answer;
 }
