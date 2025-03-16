9f54be7ffaeb8b56a2f00d69b23d328713250927
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-03-16 10:39:41 +0900

    [Silver I] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -1,43 +1,40 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-int n, m, l = 1, r, ret = 987654321;
-vector<int> v;
+int n, m, ret = 987654321, l, r;
+int arr[100001];
 
 bool check(int mid) {
     int j = 0;
-    for(int i  = 0; i < m; i++) {
+    for(int i = 0; i < m; i++) {
         int sum = 0;
-        while(j < v.size()) {
-            if(sum + v[j] > mid) {
+        while(j < n) {
+            if(sum + arr[j] > mid) {
                 break;
             }
-            sum += v[j];
+            sum += arr[j];
             j++;
         }
     }
-    return j == v.size();
+    return j == n;
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
-        v.push_back(temp);
-        r += temp;
+        cin >> arr[i];
+        r += arr[i];
     }
     while(l <= r) {
         int mid = (l + r) / 2;
         if(check(mid)) {
             ret = min(ret, mid);
             r = mid - 1;
-        }
-        else {
+        } else {
             l = mid + 1;
         }
     }
