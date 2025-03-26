7facbd7b94cbb91cf8d133e13c2cc5f9a1282cda
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-03-26 13:31:40 +0900

    [Gold IV] Title: 인구 이동, Time: 76 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,52 +1,71 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
 using namespace std;
-int visited[54][54], a[54][54], n, l, r, sum, cnt; 
-const int dy[]={-1,0,1,0};
-const int dx[] ={0,1,0,-1}; 
-vector<pair<int,int>>v;
-void dfs(int y,int x,vector<pair<int,int>> &v){ 
-    for(int i=0; i<4; i++){
-        int ny = y+dy[i];
-        int nx = x+dx[i];
-        if(nx<0 || nx>=n || ny<0 || ny>=n || visited[ny][nx])continue;
-        if(abs(a[ny][nx]- a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r){
-            visited[ny][nx] =1;
-            v.push_back({ny,nx});
-            sum += a[ny][nx];
-            dfs(ny,nx,v);
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+int n, r, l, ret, sum;
+int arr[51][51], visited[51][51];
+bool flag = true;
+vector<pair<int, int>> v;
+
+void dfs(int y, int x) {
+    visited[y][x] = 1;
+    v.push_back({y, x});
+    sum += arr[y][x];
+    for(int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx]) {
+            continue;
+        }
+        int temp = abs(arr[ny][nx] - arr[y][x]);
+        if(temp >= r && temp <= l) {
+            dfs(ny, nx);
         }
     }
 }
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-int main(){ 
-    cin>>n>>l>>r;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin>>a[i][j];
+    cin >> n >> r >> l;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
     }
-    while(true){
-        bool flag =0;
-        fill(&visited[0][0], &visited[0][0] + 54 * 54, 0);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(!visited[i][j]){
-                    v.clear();
-                    visited[i][j] = 1;
-                    v.push_back({i,j});
-                    sum = a[i][j];
-                    dfs(i,j,v);
-                    if(v.size() == 1) continue;  
-                    for(pair<int,int> b : v){ 
-                        a[b.first][b.second] = sum / v.size();
-                        flag = 1;
+
+    while(1) {
+        flag = true;
+        memset(visited, 0, sizeof(visited));
+        v.clear();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j]) {
+                    sum = 0;
+                    dfs(i, j);
+                    if(v.size() >= 2) {
+                        int temp = sum / v.size();
+                        for(auto a : v) {
+                            arr[a.first][a.second] = temp;
+                        }
+                        flag = false;
                     }
-                } 
+                    v.clear();
+                }
             }
         }
-        if(!flag) break;  
-        cnt++;
-    } 
-    cout<< cnt << "\n";
+        if(flag) {
+            break;
+        }
+        ret++;
+    }
+    cout << ret << '\n';
     return 0;
 }
