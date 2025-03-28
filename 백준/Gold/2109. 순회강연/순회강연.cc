433fee3a75709c4eb951446ceb29bc21101f982a
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-03-28 19:55:13 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
+int n, p, d, ret;
 vector<pair<int, int>> v;
 priority_queue<int, vector<int>, greater<int>> pq;
-int n, day, pay, ret;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
     cin >> n;
     for(int i = 0; i < n; i++) {
-        cin >> pay >> day;
-        v.push_back({day, pay});
+        cin >> p >> d;
+        v.push_back({d, p});
     }
     sort(v.begin(), v.end());
     for(int i = 0; i < n; i++) {
