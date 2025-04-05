53ecd0e97687c6b9082e6a02bab1e3c458b6f05c
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-04-05 22:41:01 +0900

    [Gold I] Title: 낚시왕, Time: 124 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,94 +1,85 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
+#include <algorithm>
 using namespace std;
 
 int dy[] = {-1, 1, 0, 0};
 int dx[] = {0, 0, 1, -1};
-int r, c, m, ret, h = -1;
 struct Shark {
-    int r, c, s, d, z;
+    int y, x, s, d, z;
     bool die;
 } shark[10001];
-int temp[101][101];
-
-
-void move(int idx) {
-    int y = shark[idx].r;
-    int x = shark[idx].c;
-    int cnt = shark[idx].s;
-    int dir = shark[idx].d;
-    int size = shark[idx].z;
-    int cycle;
-    if(dir <= 1) {
-        cycle = 2 * (r - 1);
-    }
-    else {
-        cycle = 2 * (c - 1);
-    }
-    if(cycle > 0) {
-        cnt %= cycle;
-    }
-    while(cnt--) {
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
-        if(ny < 0 || nx < 0 || ny >= r || nx >= c) {
-            dir ^= 1;
-            ny = y + dy[dir];
-            nx = x + dx[dir];
-        }
-        y = ny;
-        x = nx;
-    }
-    shark[idx].r = y;
-    shark[idx].c = x;
-    shark[idx].d = dir;
-
-    if(temp[y][x] != -1) {
-        if(shark[temp[y][x]].z < size) {
-            shark[temp[y][x]].die = true;
-            temp[y][x] = idx;
-        }
-        else {
-            shark[idx].die = true;
-        }
-    }
-    else {
-        temp[y][x] = idx;
-    }
-    return;
-}
+int r, c, m, ret;
+int arr[101][101], temp[101][101];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> r >> c >> m;
-    for(int i = 0; i < m; i++) {
-        cin >> shark[i].r >> shark[i].c >> shark[i].s >> shark[i].d >> shark[i].z;
-        shark[i].r--;
-        shark[i].c--;
-        shark[i].d--;
+
+    for(int i = 1; i <= m; i++) {
+        int y, x, s, d, z;
+        cin >> y >> x >> s >> d >> z;
+        y--, x--, d--;
+        shark[i].y = y; shark[i].x = x; shark[i].s = s; shark[i].d = d; shark[i].z = z;
+
+        if(shark[i].d <= 1) {
+            shark[i].s %= (2 * (r - 1));
+        }
+        else {
+            shark[i].s %= (2 * (c - 1));
+        }
+        arr[y][x] = i;
     }
-    while(h < c) {
-        h++;
-        bool flag = false;
-        for(int i = 0; i < r; i++) {
-            if(flag) {
+
+    for(int t = 0; t < c; t++) {
+        for(int y = 0; y < r; y++) {
+            if(arr[y][t]) {
+                shark[arr[y][t]].die = true;
+                ret += shark[arr[y][t]].z;
+                arr[y][t] = 0;
                 break;
             }
-            for(int j = 0; j < m; j++) {
-                if(!shark[j].die && shark[j].r == i && shark[j].c == h) {
-                    ret += shark[j].z;
-                    shark[j].die = true;
-                    flag = true;
-                    break;
+        }
+        memset(temp, 0, sizeof(temp));
+        for(int i = 1; i <= m; i++) {
+            if(shark[i].die){
+                continue;
+            }
+            int y = shark[i].y;
+            int x = shark[i].x;
+            int s = shark[i].s;
+            int d = shark[i].d;
+            int ny, nx;
+            while(s--) {
+                ny = y + dy[d];
+                nx = x + dx[d];
+                if(ny < 0 || nx < 0 || ny >= r || nx >= c) {
+                    d ^= 1;
+                    ny = y + dy[d];
+                    nx = x + dx[d];
+                }
+                y = ny;
+                x = nx;
+            }
+            shark[i].y = y;
+            shark[i].x = x;
+            shark[i].d = d;
+
+            if(temp[y][x]) {
+                if(shark[temp[y][x]].z < shark[i].z) {
+                    shark[temp[y][x]].die = true;
+                    temp[y][x] = i;
+                }
+                else {
+                    shark[i].die = true;
                 }
             }
-        }
-        memset(temp, -1, sizeof(temp));
-        for(int i = 0; i < m; i++) {
-            if(!shark[i].die) {
-                move(i);
+            else {
+                temp[y][x] = i;
             }
         }
+        memcpy(arr, temp, sizeof(arr));
     }
     cout << ret << '\n';
     return 0;
