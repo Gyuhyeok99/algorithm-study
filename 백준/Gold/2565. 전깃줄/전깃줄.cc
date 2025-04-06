00f341351e573ed3bfda1ea2799351243c88ccd8
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-04-06 18:25:51 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
 
-
-int n, a, b, len, lis[501];
-vector<pair<int, int>> v;
+int n;
+vector<pair<int ,int>> v;
+int lis[101], len;
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
     for(int i = 0; i < n; i++) {
+        int a, b;
         cin >> a >> b;
         v.push_back({a, b});
     }
     sort(v.begin(), v.end());
-    for(int i = 0; i < v.size(); i++) {
-        auto it = lower_bound(lis, lis + len, v[i].second);
-        if(*it == 0) {
+    for(int i = 0; i < n; i++) {
+        auto lowerPos = lower_bound(lis, lis + len, v[i].second);
+        if(*lowerPos == 0) {
             len++;
         }
-        *it = v[i].second;
+        *lowerPos = v[i].second;
     }
     cout << n - len << '\n';
     return 0;
