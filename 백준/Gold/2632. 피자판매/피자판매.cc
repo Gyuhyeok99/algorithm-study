d612f51766987e271c1f2939608263e48d0f97d5
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-04-06 15:01:01 +0900

    [Gold II] Title: 피자판매, Time: 476 ms, Memory: 66464 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.cc
+++ 백준/Gold/2632. 피자판매/피자판매.cc
@@ -2,54 +2,62 @@
 #include <map>
 using namespace std;
 
-int x, n, m, ret;
+int s, m, n, ret;
 int a[1001], b[1001], aSum[2002], bSum[2002];
 map<int, int> aCnt, bCnt;
-
-void make(int t, int psum[], map<int, int>& mp) {
-    for(int i = 1; i <= t; i++) {
-        for(int j = i; j < t + i; j++) {
-            int sum = psum[j] - psum[j - i];
-            mp[sum]++;
-            if(i == t) {
-                break;
-            }
-        }
-    }
-}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> x;
-    cin >> n >> m;
-    for(int i = 0; i < n; i++) {
+    cin >> s;
+    cin >> m >> n;
+
+    for(int i = 0; i < m; i++) {
         cin >> a[i];
     }
-    for(int i = 0; i < m; i++) {
+    for(int i = 0 ; i < n; i++) {
         cin >> b[i];
     }
 
-    for(int i = 1; i <= n; i++) {
+    for(int i = 1; i <= m; i++) {
         aSum[i] = aSum[i - 1] + a[i - 1];
     }
-    for(int i = 1; i <= m; i++) {
+    for(int i = m + 1; i <= 2 * m; i++) {
+        aSum[i] = aSum[i - 1] + a[i - 1 - m];
+    }
+
+    for(int i = 1; i <= n; i++) {
         bSum[i] = bSum[i - 1] + b[i - 1];
     }
-
     for(int i = n + 1; i <= 2 * n; i++) {
-        aSum[i] = aSum[i - 1] + a[i - 1 - n];
-    }
-    for(int i = m + 1; i <= 2 * m; i++) {
-        bSum[i] = bSum[ i -1] + b[i - 1 - m];
+        bSum[i] = bSum[i - 1] + b[i - 1 - n];
     }
 
-    make(n, aSum, aCnt);
-    make(m, bSum, bCnt);
-    ret += (aCnt[x] + bCnt[x]);
-    for(int i = 1; i < x; i++) {
-        ret += (aCnt[i] * bCnt[x - i]);
+    for(int i = 1; i <= m; i++) {
+        for(int j = i; j <= m + i - 1; j++) {
+            int sum = aSum[j] - aSum[j - i];
+            aCnt[sum]++;
+            if(i == m) {
+                break;
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = i; j <= n + i - 1; j++) {
+            int sum = bSum[j] - bSum[j - i];
+            bCnt[sum]++;
+            if(i == n) {
+                break;
+            }
+        }
+    }
+
+    ret = aCnt[s] + bCnt[s];
+    for(int i = 1; i < s; i++) {
+        ret += aCnt[i] * bCnt[s - i];
     }
     cout << ret << '\n';
+
     return 0;
 }
