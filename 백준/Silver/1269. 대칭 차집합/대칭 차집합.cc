709be1cc25caba749e571a8a44ba77ca14b50e81
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-04-06 17:20:47 +0900

    [Silver IV] Title: 대칭 차집합, Time: 232 ms, Memory: 20768 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,17 +1,27 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
-int n, m, cnt, temp;
-map<int, int> _map;
-int main(){
-	cin >> n >> m; 
-	for(int i = 0; i < n; i++){
-		cin >> temp; _map[temp]++; 
-	}
-	for(int i = 0; i < m; i++){
-		cin >> temp; _map[temp]++; 
-	}
-	for(auto it : _map){
-		if(it.second == 1)cnt++; 
-	}
-	cout << cnt << "\n"; 
+
+int n , m, num, ret;
+map<int, int> mp;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        cin >> num;
+        mp[num]++;
+    }
+    for(int i = 0; i < m; i++) {
+        cin >> num;
+        mp[num]++;
+    }
+    for(auto it : mp) {
+        if(it.second == 1) {
+            ret++;
+        }
+    }
+    cout << ret << '\n';
+    return 0;
 }
