9e071e10e5d195a19b1597e428bff8b4a57e917e
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-04-07 10:42:54 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,53 +1,68 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
-int n;
-int a[21][21], dp[21][21][3];
-bool check(int y, int x, int d) {
-    if(d == 0 || d == 2) {
-        if(!a[y][x]) {
-            return true;
+int n, ret;
+int arr[17][17], dp[17][17][3];
+
+bool check(int y, int x, int dir) {
+    if(dir == 0 || dir == 2) {
+        if(arr[y][x]) {
+            return false;
         }
     }
-    else if(d == 1) {
-        if(a[y][x] == 0 && a[y - 1][x] == 0 && a[y][x - 1] == 0) {
-            return true;
+    else {
+        if(arr[y][x] || arr[y - 1][x] || arr[y][x - 1]) {
+            return false;
         }
     }
-    return false;
+    return true;
+
 }
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> a[i][j];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
     }
 
-    dp[1][2][0] = 1;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            if(check(i, j + 1, 0))dp[i][j + 1][0] += dp[i][j][0];
-            if(check(i + 1, j + 1, 1))dp[i + 1][j + 1][1] += dp[i][j][0];
+    dp[0][1][0] = 1;
 
-            if(check(i + 1, j, 2))dp[i + 1][j][2] += dp[i][j][2];
-            if(check(i + 1, j + 1, 1))dp[i + 1][j + 1][1] += dp[i][j][2];
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j < n; j++) {
+            if(check(i, j + 1, 0)) {
+                dp[i][j + 1][0] += dp[i][j][0];
+            }
+            if(check(i + 1, j + 1, 1)) {
+                dp[i + 1][j + 1][1] += dp[i][j][0];
+            }
 
-            if(check(i, j + 1, 0))dp[i][j + 1][0] += dp[i][j][1];
-            if(check(i + 1, j, 2))dp[i + 1][j][2] += dp[i][j][1];
-            if(check(i + 1, j + 1, 1))dp[i + 1][j + 1][1] += dp[i][j][1];
+            if(check(i + 1, j, 2)) {
+                dp[i + 1][j][2] += dp[i][j][2];
+            }
+            if(check(i + 1, j + 1, 1)) {
+                dp[i + 1][j + 1][1] += dp[i][j][2];
+            }
+
+            if(check(i, j + 1, 0)) {
+                dp[i][j + 1][0] += dp[i][j][1];
+            }
+            if(check(i + 1, j, 2)) {
+                dp[i + 1][j][2] += dp[i][j][1];
+            }
+            if(check(i + 1, j + 1, 1)) {
+                dp[i + 1][j + 1][1] += dp[i][j][1];
+            }
         }
     }
-
-    int ret = 0;
     for(int i = 0; i < 3; i++) {
-        ret += dp[n][n][i];
+        ret += dp[n - 1][n - 1][i];
     }
     cout << ret << '\n';
     return 0;
