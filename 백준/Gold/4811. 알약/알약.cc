8068c6b80ad12803036450281be3485b4f098f9d
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-04-07 13:30:56 +0900

    [Gold V] Title: 알약, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.cc
+++ 백준/Gold/4811. 알약/알약.cc
@@ -1,31 +1,26 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int n;
 long long dp[31][31];
-
-long long solve(int a, int b) {
-    if(a == 0 && b == 0) {
+long long solve(int w, int h) {
+    if(w < 0 || h < 0) {
+        return 0;
+    }
+    if(w == 0 && h == 0) {
         return 1;
     }
-
-    if(dp[a][b]) {
-        return dp[a][b];
+    long long& ret = dp[w][h];
+    if(ret) {
+        return ret;
     }
-    long long & ret = dp[a][b];
-    if(a > 0) {
-        ret += solve(a - 1, b + 1);
-    }
-    if(b > 0) {
-        ret += solve(a, b - 1);
-    }
+    ret += solve(w - 1, h + 1) + solve( w, h - 1);
     return ret;
 }
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+    cin.tie(NULL); cout.tie(NULL);
     while(1) {
         cin >> n;
         if(n == 0) {
