076dfafdc5d4410033a47d05f249b9e2788d364f
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-06-21 18:25:40 +0900

    [level 2] Title: 완전범죄, Time: 0.20 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,28 +1,6 @@
 class Solution {
-    static int maxA, savedA;
-    static int[] dp;
     public int solution(int[][] info, int n, int m) {
         int answer = 0;
-        for (int i = 0; i < info.length; i++) {
-            maxA += info[i][0];
-        }
-        dp = new int[m];
-        for (int i = 0; i < info.length; i++) {
-            int a = info[i][0];
-            int b = info[i][1];
-            for (int j = m - 1; j >= b; j--) {
-                dp[j] = Math.max(dp[j], dp[j - b] + a);
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            savedA = Math.max(savedA, dp[i]);
-        }
-        if(maxA - savedA >= n) {
-            answer = -1;
-        }
-        else {
-            answer = maxA - savedA;
-        }
         return answer;
     }
 }
