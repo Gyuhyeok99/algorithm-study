d95b409734be3cd06b7187d91f0e8fa7c2144e1e
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-07-09 12:27:57 +0900

    [Silver II] Title: 유기농 배추, Time: 336 ms, Memory: 31052 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,19 +1,11 @@
 import java.util.*;
 
 public class Main {
-
-    private static int[] dy = {-1, 0, 1, 0};
-    private static int[] dx = {0, 1, 0, -1};
-    private static class Pair {
-        int y, x;
-        public Pair(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-    private static int t, n, m, num, ret;
-    private static int[][] arr, visited;
-
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int t, m, n, k, ret;
+    static int[][] a;
+    static boolean[][] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         t = sc.nextInt();
@@ -22,35 +14,34 @@ public class Main {
             ret = 0;
             m = sc.nextInt();
             n = sc.nextInt();
-            num = sc.nextInt();
-            arr = new int[n][m];
-            visited = new int[n][m];
-            for(int i = 0; i < num; i++) {
-                int y, x;
-                x = sc.nextInt();
-                y = sc.nextInt();
-                arr[y][x] = 1;
+            k = sc.nextInt();
+            a = new int[n][m];
+            visited = new boolean[n][m];
+            for(int i = 0; i < k; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                a[y][x] = 1;
             }
             for(int i = 0; i < n; i++) {
                 for(int j = 0; j < m; j++) {
-                    if(visited[i][j] == 0 && arr[i][j] == 1) {
-                        dfs(i, j);
+                    if(!visited[i][j] && a[i][j] == 1) {
                         ret++;
+                        dfs(i, j);
                     }
                 }
             }
             System.out.println(ret);
         }
     }
-    private static void dfs(int y, int x) {
-        visited[y][x] = 1;
+    static void dfs(int y, int x) {
+        visited[y][x] = true;
         for(int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
             if(ny < 0 || nx < 0 || ny >= n || nx >= m) {
                 continue;
             }
-            if(visited[ny][nx] == 1 || arr[ny][nx] == 0) {
+            if(a[ny][nx] == 0 || visited[ny][nx]) {
                 continue;
             }
             dfs(ny, nx);
